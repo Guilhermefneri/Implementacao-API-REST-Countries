c4d21bd7f457ecf1a46edcec6daf5aadfd772649
Guilhermefneri <guilhermefneri235@gmail.com>
2024-10-01 17:54:47 -0300

    added modal

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@ function mostrarPais(paises){
     const buscaPais = document.getElementById('buscaPais');
     buscaPais.innerHTML = paisesHTML.join(' ');
     
+    // evento de clique
     const paisesDiv = document.querySelectorAll('.paises-div')
     
-    paisesDiv.forEach(function (div){
-        div.addEventListener('click', info)
+    paisesDiv.forEach((div, index) =>{
+        div.addEventListener('click', () => info(paises[index]))
     })
 }
 
@@ -23,15 +24,45 @@ function mostrarPais(paises){
 function pegarPais(pais){
     console.log(pais)
     return `<div class = "paises-div">
-    <img id="flags" src="${pais.flags.png}" alt="${pais.flags.alt}">
-    <h2>${pais.name.common}</h2>
-    <h4>Capital: ${pais.capital}</h4>
-    <h4>Region: ${pais.region}</h4>
-    </div>`
-}
+            <img id="flags" src="${pais.flags.png}" alt="${pais.flags.alt}">
+            <h2>${pais.name.common}</h2>
+            <h4>Capital: ${pais.capital}</h4>
+            <h4>Region: ${pais.region}</h4>
+            </div>`
+        }
+        
+function info(pais){
+    const button = document.getElementById('buttonClose');
+    const modal = document.getElementById('modal');
+    const conteudo = document.getElementById('conteudo');
 
-function info(){
-    alert('teste')
-}
+    function openModal(){
+        modal.showModal();
+
+        conteudo.innerHTML = `<div class="headerModal">
+                <h2>${pais.name.common}</h2>
+                <img id="flagsModal" src="${pais.flags.png}" alt="${pais.flags.alt}">
+                </div>
+                <div class="mainModal">
+                <h4>Capital: ${pais.capital}</h4>
+                <h4>Region: ${pais.region}</h4>
+                <h4>Subregion: ${pais.subregion}</h4>
+                <h4>Population: ${pais.population}</h4>
+                <h4>Area: ${pais.area}</h4>
+                <h4>Languages: ${Object.values(pais.languages).join(', ')}</h4>
+                <h4>Timezones: ${pais.timezones.join(', ')}</h4>
+                <h4>Internet Domain (TLD Code): ${pais.tld}</h4>
+                <h4>Dialing code: ${pais.idd.root}${pais.idd.suffixes}</h4>
+                </div>
+                `
+    }
+    
+    function closeModal(){
+        modal.close();
+    }
+
+    openModal()
+    button.addEventListener('click', closeModal)
+            }
 
 countryApi();
